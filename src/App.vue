09d27e5bfbc9397fb8bcfc4d2d5d0c10cb4bbb88
L7DDummy <163566013+L7DDummy@users.noreply.github.com>
2024-04-02 20:41:04 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -111,7 +111,7 @@ export default {
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "IPS-N",
           "frame": "Lancaster",
-          "mech": "G.A.L.E"
+          "mech": "GALE"
         },
         {
           "callsign": "Pilot2",
